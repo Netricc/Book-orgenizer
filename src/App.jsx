64b6d550f53d8e9adece2a52a2578e60346a64ee
netric <nawabsn911@gmail.com>
2024-10-21 13:56:17 +0200

    init files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header.jsx"
+import LandingPage from "./LandingPage.jsx"
+
+function App() {
+
+  return (
+   <>
+   <Header></Header>
+   <LandingPage></LandingPage>
+   </>
+  )
+}
+
+export default App
